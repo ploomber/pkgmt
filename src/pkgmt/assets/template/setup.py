d21f2408f32dbe0d63eb2d13c3368b3f2c075ab5
Eduardo Blancas Reyes <github@blancas.io>
2022-02-21 10:35:21 -0600

    adds manifest.in to include pkg template assets

--- src/pkgmt/assets/template/setup.py
+++ src/pkgmt/assets/template/setup.py
@@ -28,7 +28,7 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    package_data={"": ["*.txt", "*.rst"]},
+    include_package_data=True,
     classifiers=[],
     keywords=[],
     install_requires=REQUIRES,
