0a73b2908249138d918f810dfce711e6cdb3d2d7
Neelasha Sen <neelashasen@192.168.1.2>
2022-07-08 15:59:42 +0530

    Removed incorrect line

--- src/pkgmt/config.py
+++ src/pkgmt/config.py
@@ -8,9 +8,8 @@ def load():
         with open('pyproject.toml') as f:
             return toml.load(f)['tool']['pkgmt']
     elif Path('config.yaml').exists():
-        with open('config.yaml').exists():
-            with open("config.yaml", "r") as stream:
-                try:
-                    return yaml.safe_load(stream)['pkgmt']
-                except yaml.YAMLError as exc:
-                    print("Error loading config.yaml file : {}".format(exc))
+        with open("config.yaml", "r") as stream:
+            try:
+                return yaml.safe_load(stream)['pkgmt']
+            except yaml.YAMLError as exc:
+                print("Error loading config.yaml file : {}".format(exc))
