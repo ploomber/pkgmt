f249e49f0f63e641f24ee82e4194a9e965a7047b
Eduardo Blancas Reyes <github@blancas.io>
2022-07-22 11:28:56 -0500

    closes #9

---
+++ tests/test_test.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from pkgmt import test
+
+
+def test_markdown(tmp_empty):
+    Path('file.md').write_text("""
+```python
+1 + 1
+```
+""")
+    nb = test.markdown('file.md', inplace=True)
+
+    assert nb.cells[0].outputs[0]['data']['text/plain'] == '2'
+
+
+def test_markdown_in_tmp_directory(tmp_empty):
+    Path('some-file.txt').write_text('hello!')
+
+    Path('file.md').write_text("""
+```python
+from pathlib import Path
+print(Path('some-file.txt').read_text())
+```
+""")
+    nb = test.markdown('file.md', inplace=False)
+
+    assert nb.cells[0].outputs[0]['text'] == 'hello!\n'
