ccbc9acfff954e6bed39d536544020746243f394
Eduardo Blancas <github@blancas.io>
2023-01-15 12:44:02 -0300

    deprecation finder

---
+++ src/pkgmt/deprecation.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+import re
+from glob import iglob
+from collections import defaultdict
+
+from pkgmt.versioner.versionersetup import VersionerSetup
+from pkgmt.versioner.util import complete_version_string
+from pkgmt.exceptions import ProjectValidationError
+
+
+class Deprecations:
+    def __init__(self) -> None:
+        versioner = VersionerSetup()
+        self.current = complete_version_string(
+            versioner.current_version().replace("dev", "")
+        )
+
+    def check(self):
+        """Check if there are pending deprecations"""
+        deprecations = find_deprecations()
+
+        mapping = defaultdict(lambda: [])
+
+        for dep in deprecations:
+            mapping[complete_version_string(dep.version)].append(dep)
+
+        if self.current in mapping:
+            matches_out = "\n".join(f"- {item}" for item in mapping[self.current])
+            raise ProjectValidationError(
+                f"Found the following pending deprecations:\n{matches_out}"
+            )
+
+
+class DeprecationItem:
+    def __init__(self, body, version, path) -> None:
+        self.body = body
+        self.version = version
+        self.path = str(Path(path))
+
+    def __eq__(self, o: object) -> bool:
+        return (
+            self.body == o.body
+            and self.version == o.version
+            and Path(self.path) == Path(o.path)
+        )
+
+    def __hash__(self) -> int:
+        return hash((self.body, self.version, self.path))
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.body!r}, {self.version!r}, {self.path!r})"
+
+    def __str__(self) -> str:
+        return f"{self.body!r} at {self.path!r}"
+
+
+def _find_deprecations_in_text(text):
+    """Find and parse `.. deprecated::` directives in text
+
+    Returns
+    -------
+    bodies : list
+        Content of each `.. deprecated::` directive
+
+    versions : list
+        Versions parsed from the body of each directive
+    """
+    # .. deprecated:: {version} [whitespace]
+    # [word chars or space] [version descriptor] [word chars or space]
+    bodies = re.findall(
+        r"\.\. deprecated::\s+[0-9\.]+\s+([\w,.` \n\-]+\ +[0-9\.]+[\w,`. \n\-]*\n{1})",
+        text,
+    )
+
+    versions = [re.search(r"[0-9\.]{3,5}", body).group() for body in bodies]
+
+    return bodies, versions
+
+
+def find_deprecations(root_dir=None):
+    deprecations = []
+
+    root_dir = root_dir or "."
+
+    for path in iglob(f"{root_dir}/**/*.py", recursive=True):
+        bodies, version = _find_deprecations_in_text(Path(path).read_text())
+
+        for body, version in zip(bodies, version):
+            deprecations.append(DeprecationItem(body, version, path))
+
+    return deprecations
---
+++ tests/test_deprecation.py
@@ -0,0 +1,208 @@
+from pathlib import Path
+
+import pytest
+
+from pkgmt import deprecation
+from pkgmt.exceptions import ProjectValidationError
+
+
+@pytest.mark.parametrize(
+    "text, body, version",
+    [
+        [
+            '''
+def function():
+    """
+    Notes
+    -----
+    .. deprecated:: 1.2.3
+
+        -Attention, `module.function` removed in 1.3
+    """
+    pass
+''',
+            ["-Attention, `module.function` removed in 1.3\n"],
+            ["1.3"],
+        ],
+        [
+            '''
+def function():
+    """
+    Notes
+    -----
+    .. deprecated:: 1.2.3
+        Stuff removed in 1.3, -update `module.function`
+    """
+    pass
+''',
+            ["Stuff removed in 1.3, -update `module.function`\n"],
+            ["1.3"],
+        ],
+        [
+            '''
+def function():
+    """
+    Notes
+    -----
+    .. deprecated:: 1.2.3
+        This
+        description spans
+        multiple lines. Removed in 1.3.4
+        so update
+    """
+    pass
+''',
+            (
+                [
+                    "This\n        description spans\n        "
+                    "multiple lines. Removed in 1.3.4\n        so update\n"
+                ]
+            ),
+            ["1.3.4"],
+        ],
+        [
+            '''
+def function():
+    """
+    Notes
+    -----
+    .. deprecated:: 1.2.3
+        Will be removed in 1.3
+    """
+    pass
+
+def another():
+    """
+    Notes
+    -----
+    .. deprecated:: 1.2.6
+        Will be removed in 1.4
+    """
+    pass
+''',
+            ["Will be removed in 1.3\n", "Will be removed in 1.4\n"],
+            ["1.3", "1.4"],
+        ],
+    ],
+    ids=[
+        "simple",
+        "trailing-text",
+        "multi-line-directive",
+        "multiple",
+    ],
+)
+def test_find_deprecations_in_text(text, body, version):
+    body_returned, version_returned = deprecation._find_deprecations_in_text(text)
+    assert body_returned == body
+    assert version_returned == version
+
+
+def test_find_deprecations(tmp_empty):
+    Path("some/nested/dir").mkdir(parents=True)
+
+    Path("some/nested/dir/functions.py").write_text(
+        '''
+
+def stuff():
+    """
+    Notes
+    -----
+    .. deprecated:: 0.5.6
+        Removed in 0.6
+    """
+    pass
+
+def more_stuff():
+    """
+    Notes
+    -----
+    .. deprecated:: 0.5.6
+        Removed in 0.7.0
+    """
+    pass
+'''
+    )
+
+    Path("some/nested/functions.py").write_text(
+        '''
+
+def stuff():
+    """
+    Notes
+    -----
+    .. deprecated:: 0.7
+        Removed in 0.8
+    """
+    pass
+'''
+    )
+
+    Path("functions.py").write_text(
+        '''
+
+def stuff():
+    """
+    Notes
+    -----
+    .. deprecated:: 0.9
+        Removed in 0.9.0
+    """
+    pass
+'''
+    )
+
+    expected = {
+        deprecation.DeprecationItem(
+            "Removed in 0.6\n", "0.6", "some/nested/dir/functions.py"
+        ),
+        deprecation.DeprecationItem(
+            "Removed in 0.7.0\n", "0.7.0", "some/nested/dir/functions.py"
+        ),
+        deprecation.DeprecationItem(
+            "Removed in 0.8\n", "0.8", "some/nested/functions.py"
+        ),
+        deprecation.DeprecationItem("Removed in 0.9.0\n", "0.9.0", "functions.py"),
+    }
+
+    assert set(deprecation.find_deprecations(root_dir=".")) == expected
+
+
+def test_check(backup_package_name):
+    Path("src", "package_name", "__init__.py").write_text(
+        """
+__version__ = "0.9dev"
+"""
+    )
+
+    Path("src/package_name/functions.py").write_text(
+        '''
+
+def stuff():
+    """
+    Notes
+    -----
+    .. deprecated:: 0.8.1
+        Removed in 0.9.0
+    """
+    pass
+
+
+def more_stuff():
+    """
+    Notes
+    -----
+    .. deprecated:: 0.8.2
+        Also removed in 0.9
+    """
+    pass
+'''
+    )
+
+    dep = deprecation.Deprecations()
+
+    with pytest.raises(ProjectValidationError) as excinfo:
+        dep.check()
+
+    assert "Found the following pending deprecations" in str(excinfo.value)
+    assert "Removed in 0.9.0" in str(excinfo.value)
+    assert "Also removed in 0.9" in str(excinfo.value)
