1990641b75beb6fcf8edb48e601169a163db5085
Eduardo Blancas <github@blancas.io>
2023-04-28 09:40:29 -0600

    adds missing comment in setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ REQUIRES = [
     "nbqa",
     "flake8",
     "jupytext",
+    # ensure we have a valid IPython version since
+    # black needs it
     "ipython<=8.12.0; python_version == '3.8'",
 ]
 
