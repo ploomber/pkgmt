8649aba22a2d2a82c97d7e38834a5069c95ab708
Eduardo Blancas <github@blancas.io>
2023-05-19 12:28:27 -0600

    adds test

--- tests/test_new.py
+++ tests/test_new.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import subprocess
 from pkgmt import new
 
@@ -14,3 +15,11 @@ def test_package(tmp_empty, uninstall):
     new.package("somepkg")
 
     subprocess.check_call(["pip", "install", "somepkg/"])
+
+    pyproject = Path("somepkg", "pyproject.toml").read_text()
+    setup = Path("somepkg", "setup.py").read_text()
+
+    assert 'github = "ploomber/somepkg"' in pyproject
+    assert 'package_name = "somepkg"' in pyproject
+    assert 'env_name = "somepkg"' in pyproject
+    assert 'name="somepkg"' in setup
