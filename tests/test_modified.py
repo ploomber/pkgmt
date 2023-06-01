b2667bb7f46e54b86b3023720200eed4d9fb4c22
Mohit Mehta <mehtamohit013@gmail.com>
2023-06-01 12:00:46 -0400

    Renaming

--- tests/test_modified.py
+++ tests/test_modified.py
@@ -1,5 +1,5 @@
 import pytest
-from pkgmt import modified
+from pkgmt import fail_if_modified
 
 
 @pytest.mark.parametrize(
@@ -18,8 +18,11 @@ from pkgmt import modified
     ],
 )
 def test_check_modified(tmp_package_modi, base_branch, exclude_path, returncode):
-    assert modified.check_modified(base_branch, exclude_path, debug=True) == returncode
+    assert (
+        fail_if_modified.check_modified(base_branch, exclude_path, debug=True)
+        == returncode
+    )
 
 
 def test_check_modified_2(tmp_package_modi_2):
-    assert modified.check_modified("main", ["doc"], debug=True) == 0
+    assert fail_if_modified.check_modified("main", ["doc"], debug=True) == 0
