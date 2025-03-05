87aa8c047b661284512693ef28b6b2f18bbab3f2
Eduardo Blancas <github@blancas.io>
2025-03-05 09:30:26 -0600

    aliased group

--- src/pkgmt/assets/template/src/package_name/cli.py
+++ src/pkgmt/assets/template/src/package_name/cli.py
@@ -5,9 +5,33 @@ Sample CLI (requires click, tested with click==8.1.3)
 import click
 
 
-@click.group()
+class AliasedGroup(click.Group):
+    """
+    Allow running commands by only typing the first few characters.
+    https://click.palletsprojects.com/en/8.1.x/advanced/#command-aliases
+
+    To disable, remove the `cls=AliasedGroup` argument from the `@click.group()` decorator.
+    """
+
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx) if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail(f"Too many matches: {', '.join(sorted(matches))}")
+
+    def resolve_command(self, ctx, args):
+        # always return the full command name
+        _, cmd, args = super().resolve_command(ctx, args)
+        return cmd.name, cmd, args
+
+
+@click.group(cls=AliasedGroup)
 def cli():
-    """Command-line interface"""
     pass
 
 
