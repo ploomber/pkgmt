3552244d0cace62f2b51086eaa44666462288048
Eduardo Blancas <github@blancas.io>
2025-04-14 19:00:27 -0600

    update template settings.py

--- src/pkgmt/assets/template/settings.py
+++ src/pkgmt/assets/template/settings.py
@@ -5,10 +5,9 @@ from pathlib import Path
 
 # load_dotenv()
 
-path_to_home = Path.home()
 
 PATH_TO_PROJECT_ROOT = Path(__file__).parent
 
-# PATH_TO_DB = path_to_home / "data" / "db.sqlite"
+# PATH_TO_DB = PATH_TO_PROJECT_ROOT / "data" / "db.sqlite"
 # LOCAL_DB_URI = f"sqlite:///{PATH_TO_DB}"
 # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
